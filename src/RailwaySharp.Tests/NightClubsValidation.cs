d0182772236d3b004a1245d1e2808c4a1bf82f05
Giacomo Stelluti Scala <gsscoder@gmail.com>
2015-07-16 12:39:20 +0200

    Adding NUnit to test project

---
+++ src/RailwaySharp.Tests/NightClubsValidation.cs
@@ -0,0 +1,118 @@
+﻿using Chessie.ErrorHandling;
+using Chessie.ErrorHandling.CSharp;
+using Microsoft.FSharp.Collections;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chessie.CSharp.Test
+{
+    // originally from https://github.com/fsprojects/fsharpx/blob/master/tests/FSharpx.CSharpTests/ValidationExample.cs
+
+    enum Sobriety { Sober, Tipsy, Drunk, Paralytic, Unconscious }
+    enum Gender { Male, Female }
+
+    class Person
+    {
+        public Gender Gender { get; private set; }
+        public int Age { get; private set; }
+        public List<string> Clothes { get; private set; }
+        public Sobriety Sobriety { get; private set; }
+
+        public Person(Gender gender, int age, List<string> clothes, Sobriety sobriety)
+        {
+            this.Gender = gender;
+            this.Age = age;
+            this.Clothes = clothes;
+            this.Sobriety = sobriety;
+        }
+    }
+
+    class Club
+    {
+        public static Result<Person, string> CheckAge(Person p)
+        {
+            if (p.Age < 18)
+                return Result<Person, string>.FailWith("Too young!");
+            if (p.Age > 40)
+                return Result<Person, string>.FailWith("Too old!");
+            return Result<Person, string>.Succeed(p);
+        }
+
+        public static Result<Person, string> CheckClothes(Person p)
+        {
+            if (p.Gender == Gender.Male && !p.Clothes.Contains("Tie"))
+                return Result<Person, string>.FailWith("Smarten up!");
+            if (p.Gender == Gender.Female && p.Clothes.Contains("Trainers"))
+                return Result<Person, string>.FailWith("Wear high heels!");
+            return Result<Person, string>.Succeed(p);
+        }
+
+        public static Result<Person, string> CheckSobriety(Person p)
+        {
+            if (new[] { Sobriety.Drunk, Sobriety.Paralytic, Sobriety.Unconscious }.Contains(p.Sobriety))
+                return Result<Person, string>.FailWith("Sober up!");
+            return Result<Person, string>.Succeed(p);
+        }
+    }
+
+    class ClubbedToDeath
+    {
+        public static Result<decimal, string> CostToEnter(Person p)
+        {
+            return from a in Club.CheckAge(p)
+                   from b in Club.CheckClothes(a)
+                   from c in Club.CheckSobriety(b)
+                   select c.Gender == Gender.Female ? 0m : 5m;
+        }
+    }
+
+    [TestFixture]
+    class Test1
+    {
+        [Test]
+        public void Part1()
+        {
+            var Dave = new Person(Gender.Male, 41, new List<string> { "Tie", "Jeans" }, Sobriety.Sober);
+            var costDave = ClubbedToDeath.CostToEnter(Dave);
+            Assert.AreEqual("Too old!", costDave.FailedWith().First());
+
+            var Ken = new Person(Gender.Male, 28, new List<string> { "Tie", "Shirt" }, Sobriety.Tipsy);
+            var costKen = ClubbedToDeath.CostToEnter(Ken);
+            Assert.AreEqual(5m, costKen.SucceededWith());
+
+            var Ruby = new Person(Gender.Female, 25, new List<string> { "High heels" }, Sobriety.Tipsy);
+            var costRuby = ClubbedToDeath.CostToEnter(Ruby);
+            costRuby.Match(
+                (x, msgs) =>
+                {
+                    Assert.AreEqual(0m, x);
+                },
+                msgs =>
+                {
+                    Assert.Fail();
+
+                });
+
+            var Ruby17 = new Person(Ruby.Gender, 17, Ruby.Clothes, Ruby.Sobriety);
+            var costRuby17 = ClubbedToDeath.CostToEnter(Ruby17);
+            Assert.AreEqual("Too young!", costRuby17.FailedWith().First());
+
+            var KenUnconscious = new Person(Ken.Gender, Ken.Age, Ken.Clothes, Sobriety.Unconscious);
+            var costKenUnconscious = ClubbedToDeath.CostToEnter(KenUnconscious);
+            costKenUnconscious.Match(
+                (x, msgs) =>
+                {
+                    Assert.Fail();
+                },
+                msgs =>
+                {
+                    Assert.AreEqual("Sober up!", msgs.First());
+                });
+        }
+    }
+}
+
---
+++ src/RailwaySharp.Tests/SimpleValidation.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Chessie.ErrorHandling;
+using Chessie.ErrorHandling.CSharp;
+
+namespace Chessie.CSharp.Test
+{
+    public class Request
+    {
+        public string Name { get; set; }
+        public string EMail { get; set; }
+    }
+
+    public class Validation
+    {
+        public static Result<Request, string> ValidateInput(Request input)
+        {
+            if (input.Name == "")
+                return Result<Request, string>.FailWith("Name must not be blank");
+            if (input.EMail == "")
+                return Result<Request, string>.FailWith("Email must not be blank");
+            return Result<Request, string>.Succeed(input);
+
+        }
+    }
+
+    [TestFixture]
+    public class TrySpecs
+    {
+        [Test]
+        public void TryWillCatch()
+        {
+            var exn = new Exception("Hello World");
+            var result = Result<string, Exception>.Try(() => { throw exn; });
+            Assert.AreEqual(exn, result.FailedWith().First());
+        }
+
+        [Test]
+        public void TryWillReturnValue()
+        {
+            var result = Result<string, Exception>.Try(() => { return "hello world"; });
+            Assert.AreEqual("hello world", result.SucceededWith());
+        }
+    }
+
+    [TestFixture]
+    public class SimpleValidation
+    {
+        [Test]
+        public void CanCreateSuccess()
+        {
+            var request = new Request { Name = "Steffen", EMail = "mail@support.com" };
+            var result = Validation.ValidateInput(request);
+            Assert.AreEqual(request, result.SucceededWith());
+        }
+    }
+
+    [TestFixture]
+    public class SimplePatternMatching
+    {
+        [Test]
+        public void CanMatchSuccess()
+        {
+            var request = new Request { Name = "Steffen", EMail = "mail@support.com" };
+            var result = Validation.ValidateInput(request);
+            result.Match(
+               (x, msgs) => { Assert.AreEqual(request, x); },
+               msgs => { throw new Exception("wrong match case"); });
+        }
+
+        [Test]
+        public void CanMatchFailure()
+        {
+            var request = new Request { Name = "Steffen", EMail = "" };
+            var result = Validation.ValidateInput(request);
+            result.Match(
+               (x, msgs) => { throw new Exception("wrong match case"); },
+               msgs => { Assert.AreEqual("Email must not be blank", msgs[0]); });
+        }
+    }
+
+    [TestFixture]
+    public class SimpleEitherPatternMatching
+    {
+        [Test]
+        public void CanMatchSuccess()
+        {
+            var request = new Request { Name = "Steffen", EMail = "mail@support.com" };
+            var result =
+                Validation
+                 .ValidateInput(request)
+                 .Either(
+                   (x, msgs) => { return x; },
+                   msgs => { throw new Exception("wrong match case"); });
+            Assert.AreEqual(request, result);
+        }
+
+        [Test]
+        public void CanMatchFailure()
+        {
+            var request = new Request { Name = "Steffen", EMail = "" };
+            var result =
+               Validation.ValidateInput(request)
+                .Either(
+                   (x, msgs) => { throw new Exception("wrong match case"); },
+                   msgs => { return msgs[0]; });
+
+            Assert.AreEqual("Email must not be blank", result);
+        }
+    }
+}
